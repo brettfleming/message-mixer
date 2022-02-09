d8eefe5ca3cab26002e73206e2b7207467a54b3b
Brett Fleming <brettfleming015@gmail.com>
2022-02-09 10:54:49 -0600

    added the function the collects the data about the encryption type

--- message-mixer.js
+++ message-mixer.js
@@ -7,3 +7,28 @@ const encryptionMethod = getEncryptionMethod();
 process.stdin.on('data', (userInput) => {
   displayEncryptedMessage(encryptionMethod, userInput);
 });
+
+function getEncryptionMethod() {
+    let encryptionMethod;
+    
+    const encryptionType = process.argv[2];  
+    if (encryptionType === 'symbol') {
+      encryptionMethod = symbolCipher;
+    } else if (encryptionType === 'reverse') {
+      encryptionMethod = reverseCipher;
+    } else if (encryptionType === 'caesar') {
+      let amount = Number(process.argv[3]);
+      if (Number.isNaN(amount)) {
+        process.stdout.write(`Try again with a valid amount argument. \n`)
+        process.exit();  
+      }
+      encryptionMethod = (str) => caesarCipher(str, amount);
+    } 
+    else {
+      process.stdout.write(`Try again with a valid encryption type. \n`)
+      process.exit();
+    }
+  
+    process.stdout.write('Enter the message you would like to encrypt...\n> ');
+    return encryptionMethod;
+  }
