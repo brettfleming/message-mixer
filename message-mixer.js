f0a67eda5e2598dad41f6f28083984b1f7d12c8b
Brett Fleming <brettfleming015@gmail.com>
2022-02-09 10:50:56 -0600

    starting message-mixer.js

--- message-mixer.js
+++ message-mixer.js
@@ -1,1 +1,9 @@
 const encryptors = require('./encryptors.js');
+const {caesarCipher, symbolCipher, reverseCipher} = encryptors
+// User Input / Output Logic
+/////////////////////////////////////////////
+
+const encryptionMethod = getEncryptionMethod();
+process.stdin.on('data', (userInput) => {
+  displayEncryptedMessage(encryptionMethod, userInput);
+});
