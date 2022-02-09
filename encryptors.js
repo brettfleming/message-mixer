ebde2090ecdeb4a80b5f137865f4b16ab863bde0
Brett Fleming <brettfleming015@gmail.com>
2022-02-09 10:39:35 -0600

    added both symbolChipher and reverseCipher

--- encryptors.js
+++ encryptors.js
@@ -17,13 +17,45 @@ const caesarCipher = (str, amount = 0) => {
     }
     return output;
   };
-const symbolCipher = (str) => {
-
-}
-
-const reverseCipher = (sentence) => {
-
-}
+  
+  const symbolCipher = (str) => {
+    const symbols = {
+      'i': '!',
+      '!': 'i',
+      'l': '1',
+      '1': 'l',
+      's': '$',
+      '$': 's',
+      'o': '0',
+      '0': 'o',
+      'a': '@',
+      '@': 'a',
+      'e': '3',
+      '3': 'e',
+      'b': '6',
+      '6': 'b'
+    }
+  
+    let output = '';
+    for (let i = 0; i < str.length; i++) {
+      let char = str.toLowerCase()[i];
+  
+      if (symbols[char]) {
+        output += symbols[char]
+      } else {
+        output += char;
+      }
+    }
+    return output;
+  }
+  
+  const reverseCipher = (sentence) => {
+    let words = sentence.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].split('').reverse().join('');
+    }
+     return words.join(' ');
+  };
 
 
 module.exports.ceasarCipher = ceasarCipher
